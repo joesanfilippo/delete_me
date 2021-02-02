49ac95e6006066dc8f16400071d932b5856a1e6f
Joe Sanfilippo <jsanfilippo19@gmail.com>
2021-02-02 10:54:13 -0700

    updating files

---
+++ python_practice.py
@@ -0,0 +1,17 @@
+# Write and execute python code in a .py file
+
+# Write and execute a jupyter notebook containing both code & markdown
+
+# Write code using basic python data types & their methods
+
+# Write functions
+
+# Determine type, value, and methods of an object
+
+# Use f-string or format method
+print("The sum of 1+2 is {0} and the sum of 3+4 is {1}".format(1+2,3+4))
+# list comprehensions
+
+# Use 'for' loop and 'while' loops
+
+# print('Hello World')
---
+++ python_test.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+variable_x = 1 
+print(type(variable_x))
+
+this_is_a_string = 'This is a string'
+print(type(this_is_a_string))
